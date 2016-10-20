b0e5eec294d72c9faa2fc712395d04c02df6b085
Ryan Frazier <frazierr2@gmail.com>
2016-10-20 16:20:43 -0400

    Assignment A done

--- a/app/scripts/index.js
+++ a/app/scripts/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+var Backbone = require('backbone');
+var $ = require('jquery');
+var models = require('./models/models');
+var views = require('./view/views');
+
+var blogPost = new models.PostCollection();
+// console.log(blogPost);
+
+var heading = new views.BlogInputPage();
+$('.heading').append(heading.render().el);
+
+var postForm = new views.BlogInput({
+  collection: blogPost
+});
+$('.post').append(postForm.render().el);
+
+
+blogPost.fetch();
--- a/app/scripts/view/views.js
+++ a/app/scripts/view/views.js
@@ -1,2 +1,53 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
+var postTemplate = require('../../template/postTemplate.hbs');
+
+//******HEADING******
+var BlogInputPage = Backbone.View.extend({
+  tagName: 'h1',
+  attributes: {
+    id: 'blog-header'
+  },
+  render: function() {
+    this.$el.text('Enter your new blog post below:');
+    return this;
+  }
+});
+
+//******TAKING DATA AND POSTING******
+var BlogInput = Backbone.View.extend({
+  tagName: 'form',
+  attributes: {
+    id: 'inputForm'
+  },
+  template: postTemplate,
+  events: {
+    'submit': 'addPost'
+  },
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  },
+  addPost: function(e) {
+    e.preventDefault();
+
+    var newPost = {
+      title: $('#title').val(),
+      'blog-content': $('#blog-content').val()
+    };
+    console.log(this.collection);
+    this.collection.create(newPost);
+  }
+});
+
+
+
+
+
+
+
+
+module.exports = {
+  BlogInputPage: BlogInputPage,
+  BlogInput: BlogInput
+}
